287c8d16c66902043c83e399bc2e5b8ce05a9fc2
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2023-07-28 19:10:24 +0100

    change pygame alias from p to pg

--- pgessentials/button.py
+++ pgessentials/button.py
@@ -1,11 +1,11 @@
 from .errors import InvalidFont
 from .base_class import BaseClass
-import pygame as p
+import pygame as pg
 
 
 class Button(BaseClass):
 
-    def __init__(self, size, pos, colour, text="", text_colour=p.Color("white"), font="arial", font_size=30):
+    def __init__(self, size, pos, colour, text="", text_colour=pg.Color("white"), font="arial", font_size=30):
         super().__init__(pos, size, colour)
         self.has_text = True if text else False
 
@@ -14,13 +14,13 @@ class Button(BaseClass):
             self.text_colour = text_colour
             self.font = font.lower()
             self.font_size = font_size
-            if self.font not in p.font.get_fonts():
+            if self.font not in pg.font.get_fonts():
                 raise InvalidFont(self.font)
 
     def draw(self, screen):
-        p.draw.rect(screen, self.colour, (self.x, self.y, self.width, self.height))
+        pg.draw.rect(screen, self.colour, (self.x, self.y, self.width, self.height))
         if self.has_text:
-            font = p.font.SysFont(self.font, self.font_size)
+            font = pg.font.SysFont(self.font, self.font_size)
             text = font.render(self.text, True, self.text_colour)
             screen.blit(text, (self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
 
--- pgessentials/input_field.py
+++ pgessentials/input_field.py
@@ -1,10 +1,10 @@
 from .base_class import BaseClass
 from .errors import InvalidFont
-import pygame as p
+import pygame as pg
 
 
 class InputField(BaseClass):
-    def __init__(self, pos, size, colour, active_colour, background_text="", text_colour=p.Color("white"), font="arial", font_size=30):
+    def __init__(self, pos, size, colour, active_colour, background_text="", text_colour=pg.Color("white"), font="arial", font_size=30):
         super().__init__(pos, size, colour)
         self.active_colour = active_colour
         self.text_colour = text_colour
@@ -13,18 +13,18 @@ class InputField(BaseClass):
         self.text = ""
         self.bg_text = background_text
         self.active = False
-        if self.font not in p.font.get_fonts():
+        if self.font not in pg.font.get_fonts():
             raise InvalidFont(self.font)
 
     def draw(self, screen):
-        p.draw.rect(screen, self.colour if not self.active else self.active_colour, (self.x, self.y, self.width, self.height))
-        font = p.font.SysFont(self.font, self.font_size, italic=True if not self.text else False)
+        pg.draw.rect(screen, self.colour if not self.active else self.active_colour, (self.x, self.y, self.width, self.height))
+        font = pg.font.SysFont(self.font, self.font_size, italic=True if not self.text else False)
         text = font.render(self.text, True, self.text_colour) if self.text else font.render(self.bg_text, True, self.text_colour)
         screen.blit(text, (self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
 
     def on_key_press(self, key):
         if self.active:
-            if key == p.K_BACKSPACE:
+            if key == pg.K_BACKSPACE:
                 self.text = self.text[:-1]
             else:
-                self.text += p.key.name(key)
+                self.text += pg.key.name(key)
